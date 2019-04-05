5276eda4a7cf1571abe6a97ecbc3c899ff1e7ebc
coder <nompetsheniayabonga@gmail.com>
2019-04-05 13:51:43 +0200

    added commands for my greetings app

--- src/main/java/net/greet/Greeter.java
+++ src/main/java/net/greet/Greeter.java
@@ -23,18 +23,15 @@ public class Greeter {
                 System.out.println ( "\n" + ( userName ) );
 
             } else if (arr.length == 3) {
-
                 String userName = arr[ 1 ];
                 Greet.nameS ( userName );
                 language = arr[ 2 ];
                 Language.valueOf ( language.toLowerCase ( ) );
                 System.out.println ( "\n" + Language.valueOf ( language.toLowerCase ( ) ).getGreet ( userName ) );
             }
-
             else if (arr[ 0 ].equals ( "remove".toLowerCase ()) && arr.length == 2) {
                 String userName = arr[ 1 ];
                 Greet.removeName ( userName );
-
             }
            else if (arr[ 0 ].equals ( "clear".toLowerCase ()) && arr.length == 1) {
                 Greet.clearNames ();
@@ -45,14 +42,12 @@ public class Greeter {
             else if (arr[ 0 ].equals ( "greetcount") && arr.length == 2) {
                 String userName = arr[ 1 ];
                 Greet.greetedName ( userName );
-
             }
             else if (arr[ 0 ].equals ( "counter" ) && arr.length==1) {
                 Greet.count ();
             }
             else if (arr[ 0 ].equals ( "help" ) && arr.length==1) {
                 Greet.help ();
-
             }
             else if (arr[ 0 ].equals ( "exit" ) && arr.length==1) {
                 Greet.exit ();
