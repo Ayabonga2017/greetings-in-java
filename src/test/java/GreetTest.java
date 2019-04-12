2ed3a8ca830d63a75080d04b29ebe9d810e7b636
coder <nompetsheniayabonga@gmail.com>
2019-04-12 19:09:32 +0200

    fixed my tests and removed static from my methods

--- src/test/java/GreetTest.java
+++ src/test/java/GreetTest.java
@@ -1,14 +1,15 @@
-import net.greet.Greet;
+
+import net.greet.GreetWithHashMap;
 import net.greet.Language;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreetTest {
-
+    GreetWithHashMap greetMap = new GreetWithHashMap ();
     @BeforeEach
     public  void cleanMap(){
-        Greet.clearNames (); }
+        greetMap.clearNames (); }
 
     @Test
     public void greetMe ( ){
@@ -16,7 +17,7 @@ public class GreetTest {
         System.out.println ( "\nShould greet Minentle in French :" );
 
         String userName = "MINENHLE";
-        Greet.nameS ( userName );
+        greetMap.names ( userName );
         String language =Language.valueOf ( "french".toLowerCase () ).getValue ();
 
         assertEquals (language + userName, "BONJOUR MINENHLE" );
@@ -24,7 +25,7 @@ public class GreetTest {
         System.out.println ( "\nShould greet AYA in English :" );
 
         String name = "AYA";
-        Greet.nameS ( name );
+        greetMap.names ( name );
         String languageType =Language.valueOf ( "english".toLowerCase () ).getValue ();
 
         assertEquals ( languageType + name , "HEY AYA" );
@@ -34,58 +35,58 @@ public class GreetTest {
     @Test
     public  void greetCounter(){
 
-        Greet.nameS ( "Min" );
-        Greet.nameS ( "Minen" );
-        Greet.nameS ( "Ayanda" );
-        Greet.nameS ( "Ama" );
-        Greet.nameS ( "lllll" );
-        Greet.nameS ( "Asa" );
-        Greet.nameS ( "Asa" );
+        greetMap.names ( "Min" );
+        greetMap.names ( "Minen" );
+        greetMap.names ( "Ayanda" );
+        greetMap.names ( "Ama" );
+        greetMap.names ( "lllll" );
+        greetMap.names ( "Asa" );
+        greetMap.names ( "Asa" );
 
-        assertEquals (6,Greet.count ( ));
+        assertEquals (6,greetMap.count ( ));
     }
 
     @Test
     public  void greetedNames(){
 
-        Greet.nameS ( "Min" );
-        Greet.nameS ( "Min" );
-        Greet.nameS ( "Minen" );
-        Greet.nameS ( "Ayanda" );
-        Greet.nameS ( "Ama" );
-        Greet.nameS ( "lllll" );
-        Greet.nameS ( "Asa" );
+        greetMap.names ( "Min" );
+        greetMap.names ( "Min" );
+        greetMap.names ( "Minen" );
+        greetMap.names ( "Ayanda" );
+        greetMap.names ( "Ama" );
+        greetMap.names ( "lllll" );
+        greetMap.names ( "Asa" );
         System.out.println ( "\nShould return the list of names on the map :\n" );
-        Greet.namesGreeted();
+        greetMap.namesGreeted();
     }
 
     @Test
     public  void greetRemoveUserName(){
         System.out.println ( "\nShould remove a name  and decrement the counter:\n" );
-        Greet.nameS ( "Min" );
-        Greet.nameS ( "Minen" );
-        Greet.nameS ( "Ayanda" );
-        Greet.nameS ( "Ama" );
-        Greet.nameS ( "lllll" );
-        Greet.nameS ( "Asa" );
-        Greet.namesGreeted();
-        Greet.removeName ("ASA");
-        Greet.namesGreeted();
-        assertEquals ( 5,Greet.count ());
+        greetMap.names ( "Min" );
+        greetMap.names ( "Minen" );
+        greetMap.names ( "Ayanda" );
+        greetMap.names ( "Ama" );
+        greetMap.names ( "lllll" );
+        greetMap.names ( "Asa" );
+        greetMap.namesGreeted();
+        greetMap.removeName ("ASA");
+        greetMap.namesGreeted();
+        assertEquals ( 5,greetMap.count ());
     }
 
     @Test
     public  void greetClear(){
         System.out.println ( "\nList should be empty and counter be 0:\n" );
-        Greet.nameS ( "Min" );
-        Greet.nameS ( "Minen" );
-        Greet.nameS ( "Ayanda" );
-        Greet.nameS ( "Ama" );
-        Greet.nameS ( "lllll" );
-        Greet.nameS ( "Asa" );
-        Greet.clearNames ();
+        greetMap.names ( "Min" );
+        greetMap.names ( "Minen" );
+        greetMap.names ( "Ayanda" );
+        greetMap.names ( "Ama" );
+        greetMap.names ( "lllll" );
+        greetMap.names ( "Asa" );
+        greetMap.clearNames ();
 
-        assertEquals ( 0,Greet.count () );
+        assertEquals ( 0,greetMap.count () );
     }
 
 }
