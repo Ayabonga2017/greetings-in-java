03ebb282e4da91e9223eab96c931ba2476f5b9d9
Ayabonga2017 <nompetsheniayabonga@gmail.com>
2019-05-06 11:55:52 +0200

    fixed my db tests

--- src/main/resources/db/migration/V2__Add_people.sql
+++
@@ -1,5 +0,0 @@
-  insert into people (name, counter) values ('lunga', 1);
-   insert into people (name, counter) values ('lunga', 2);
-    insert into people (name, counter) values ('lunga', 3);
-     insert into people (name, counter) values ('lungas', 1);
-      insert into people (name, counter) values ('jackson', 1);
---
+++ src/main/resources/db/migration/V2__add_people.sql
@@ -0,0 +1,3 @@
+    insert into people (name, counter) values ('lunga', 3);
+     insert into people (name, counter) values ('lungas', 1);
+      insert into people (name, counter) values ('jackson', 1);
